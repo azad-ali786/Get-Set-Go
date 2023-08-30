aac9f22df937337d0fdc237d483f3b4b0476ff8a
azad-ali786 <azad_ug@cse.nits.ac.in>
2023-08-30 14:29:06 +0530

    IfElse

---
+++ 11ifelse/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Welcome to ifelse \n")
+
+	login := true
+
+	if login {
+		fmt.Println("Heya! You are logged in")
+	}
+
+	if num :=3; num<10 {
+		fmt.Println("Number is less than 10")
+	}
+
+}
