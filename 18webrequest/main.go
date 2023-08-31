9b13c0a6fd78509a1f5ad79ef06d1cccae0967d3
azad-ali786 <azad_ug@cse.nits.ac.in>
2023-09-01 01:33:51 +0530

    Web request

---
+++ 18webrequest/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const url = "https://lco.dev"
+
+func main() {
+	fmt.Println("Welcome to Web requests")
+
+	response, err := http.Get(url)
+
+	if err!=nil {
+		panic(err)
+	}
+
+	fmt.Printf("Response is of type %T \n", response)
+
+	defer response.Body.Close() // caller's responsibility to close the the connections
+
+	dataByte, err := ioutil.ReadAll(response.Body)
+
+	if err!=nil {
+		panic(err)
+	}
+
+	fmt.Println("The data is", string(dataByte))
+}
