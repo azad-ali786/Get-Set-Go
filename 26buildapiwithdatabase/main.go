34ab1a0fa5ddf5850e2c0fb53f714f8001c2268b
azad-ali786 <azad_ug@cse.nits.ac.in>
2023-09-09 21:21:32 +0530

    26/BuildAPIwithDatabase Server added

--- 26buildapiwithdatabase/main.go
+++ 26buildapiwithdatabase/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
-"fmt"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/azad-ali786/mongoapi/router"
 )
 
 func main() {
 	fmt.Println("Welcome to Creating APIs with MongoDB")
+
+	fmt.Println("MongoDB API")
+	r := router.Router()
+	fmt.Println("Server is getting started...")
+	log.Fatal(http.ListenAndServe(":4000", r))
+	fmt.Println("Listening at port 4000 ...")
 }
