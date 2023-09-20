8f0916da565bf670bde1174f7886ec5c44a85b73
azad-ali786 <azad_ug@cse.nits.ac.in>
2023-09-20 23:24:23 +0530

    Completed and Tested Routes

--- 30bookmanagementsystem/pkg/routes/book-routes.go
+++ 30bookmanagementsystem/pkg/routes/book-routes.go
@@ -8,7 +8,7 @@ import (
 var RegisterBookStoreROutes = func(router *mux.Router){
 	router.HandleFunc("/book", controllers.CreateBook).Methods("POST")
 	router.HandleFunc("/book", controllers.GetBook).Methods("GET")
-	router.HandleFunc("/book/{bookId}", controllers.GetBookByBookId).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
 	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
 	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
 
